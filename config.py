8adc4d5bcef88c42c494eedcdb258f2955abeb98
5210411158_Muktiono Nugroho <91129825+muktiono67517@users.noreply.github.com>
2026-01-09 14:15:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'ini_secret_key_default')
 MYSQL_CONFIG = {
     'host': '127.0.0.1',      # atau 'localhost'
     'user': 'root',           # ganti dengan username MySQL-mu
-    'password': 'Mukti01!!',  # ganti dengan password MySQL-mu
+    'password': '',  # ganti dengan password MySQL-mu
     'database': 'nikah' # ganti dengan nama database yang kamu pakai
 }
